6d0ccc72f956f0ffd223cd357a72060e8291e1e9
g1gabyt3333 <63312367+g1gabyt3333@users.noreply.github.com>
2020-11-11 13:47:45 -0500

    a

---
+++ index.js
@@ -0,0 +1,37 @@
+// var name = "Ashton";
+// var age = 16;
+// var isBoy = true;
+
+
+
+
+// //three data types: string, number, boolean
+
+// //System.out.println();
+// console.log(name);
+
+// //conditionals
+
+// if(age>18){
+
+//     console.log("You are an adult")
+
+// }
+// else{
+//     console.log("You are a child!")
+// }
+
+// for(var i = 0; i<10; i++){
+// console.log(i)
+
+// }
+
+// alert("Welcome!");
+    
+document.getElementsByTagName("h2").addEventListener("click", triggerButton);
+
+function triggerButton(){
+
+    alert("About me!");
+
+}
