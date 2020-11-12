18ee8a6c36ae04f97cd643d1d3ac9ec36557e091
g1gabyt3333 <63312367+g1gabyt3333@users.noreply.github.com>
2020-11-12 13:56:57 -0500

    awesome

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 
 
+// alert("Not ready yet")
+var test = document.getElementById("test")
+
+
+test.addEventListener("click", myFunc)
+
+function myFunc(){
+
 alert("Not ready yet")
+
+
+
+}
---
+++ osu.js
@@ -0,0 +1,3 @@
+
+var img = document.getElementById("s1")
+
