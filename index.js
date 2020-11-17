259becf15c3fe94afad328ff2c4bbacfcb32e552
g1gabyt3333 <63312367+g1gabyt3333@users.noreply.github.com>
2020-11-17 17:07:56 -0500

    morew stuff

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 
 // alert("Not ready yet")
-var test = document.getElementById("test")
+var test = document.getElementById("discord")
 
 
 test.addEventListener("click", myFunc)
 
 function myFunc(){
 
-alert("Not ready yet")
+alert("Discord: G1gabyt3#0049")
 
 
 
