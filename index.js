c9d603222bfd46b2b5788f400d95c98d63e81a29
g1gabyt3333 <63312367+g1gabyt3333@users.noreply.github.com>
2020-12-09 13:45:59 -0500

    added semi-functional dark theme toggle

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-
 // alert("Not ready yet")
 var discord = document.getElementById("discord")
 
 // document.getElementsByClassName("head")[0].innerHTML = "Hello" 
 
+
+var theme = document.getElementById("theme-toggle")
+var isDark = false;
+
+theme.addEventListener("click", toggleTheme)
 discord.addEventListener("click", myFunc)
 
 function myFunc(){
@@ -14,3 +18,42 @@ alert("Discord: G1gabyt3#0049")
 
 
 }
+
+
+function toggleTheme(){
+
+    var navbar = document.getElementsByTagName("nav");
+
+
+    console.log("Success")
+
+    if(isDark == false){
+
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white"
+        isDark = true;
+        console.log("switch to black")
+
+
+
+    }
+
+    else if (isDark == true){
+
+        console.log("switch to white")
+        document.body.style.backgroundColor = "#FFFFFF";
+        // document.getElementsById("contain").innerText.style.color = "#FFFFFF";
+        document.body.style.color = "black"
+      
+        isDark = false;
+
+
+
+    }
+
+
+
+}
+
+
+
